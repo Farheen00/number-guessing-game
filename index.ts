a325fff72d7b74d214598a3369a09a256403f2ac
Mehreen Osama <Mehreen@PGL.COM>
2024-03-30 03:11:07 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1); 
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number between 1-6:",
+  },
+]);
+//conditional statement
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("Congratulations! You have guessed the correct number.");
+} else {
+  console.log("Oops!You have guessed the wrong number.");
+}
